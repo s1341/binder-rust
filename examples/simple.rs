59ba6dbf144e9c58e0f8fd6c526b9960bb47263f
s1341 <github@shmarya.net>
2026-03-02 13:32:57 +0200

    Update example, add config.toml

--- examples/simple.rs
+++ examples/simple.rs
@@ -2,19 +2,19 @@ extern crate binder_rust;
 use binder_rust::{Parcel, ServiceManager};
 
 fn main() {
-    let mut service_manager = &mut ServiceManager::new();
+    let mut service_manager = ServiceManager::new().unwrap();
 
-    let mut package_manager = service_manager.get_service("myservice", "com.example.IMyService");
-
-    let mut parcel = Parcel::empty();
-    parcel.write_str16("Hello World");
-    let mut res = package_manager.call(1, &mut parcel);
-    println!("response: {:?}", res.read_str16());
+    let mut package_manager = service_manager.get_service("myservice", "com.example.IMyService").unwrap();
 
     let mut parcel = Parcel::empty();
-    parcel.write_str16("/data/local/tmp/testfile");
-    let mut res = package_manager.call(2, &mut parcel);
-    let fd = res.read_file_descriptor();
+    let _ = parcel.write_str16("Hello World");
+    let res = package_manager.call(1, &mut parcel);
+    println!("response: {:?}", res.unwrap().read_str16());
+
+    let mut parcel = Parcel::empty();
+    let _ = parcel.write_str16("/data/local/tmp/testfile");
+    let mut res = package_manager.call(2, &mut parcel).unwrap();
+    let fd = res.read_file_descriptor().unwrap();
     unsafe {
         nix::libc::write(fd, "Hello world".as_ptr() as *const std::ffi::c_void, 11);
     }
